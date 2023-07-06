49ac08831de84bfb74a061347b7e2674ae3c119e
aboufazel <a.ab2521@gmail.com>
2023-07-06 19:10:44 +0330

    feat :redux persist add  done

--- src/Redux-toolkit/store.js
+++ src/Redux-toolkit/store.js
@@ -2,6 +2,12 @@ import {configureStore} from "@reduxjs/toolkit";
 import counterReducer from "./slice/counterSlice";
 import categoryReducer from "./slice/categorySlice"
 
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "myApp",
+    storage,
+};
 
 const store = configureStore({
     reducer:{

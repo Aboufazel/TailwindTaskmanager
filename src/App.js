6d880481f0215bd9f9728400af5fb0d3ed89dc6c
aboufazel <a.ab2521@gmail.com>
2023-07-11 00:31:44 +0330

    feat : react Query setup done

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import store, {persistor} from "./Redux-toolkit/store";
 import TaskList from "./components/taskList/taskList";
 import {PersistGate} from "redux-persist/integration/react";
 import DoingList from "./components/doingList/doingList";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 
 function App() {
 
 
+    const queryClient = new QueryClient()
+
+
     return (
+
+
         <Provider store={store}>
             <PersistGate persistor={persistor}>
-                <div className={"grid grid-cols-12 gap-3 p-3"}>
-                    <TaskList/>
-                    <DoingList/>
-                </div>
+                <QueryClientProvider client={queryClient}>
+                    <div className={"grid grid-cols-12 gap-3 p-3"}>
+                        <TaskList/>
+                        <DoingList/>
+                    </div>
+                </QueryClientProvider>
             </PersistGate>
         </Provider>
+
     );
 }
 
--- src/components/doingList/doingList.js
+++ src/components/doingList/doingList.js
@@ -1,12 +1,54 @@
-
+import {useQuery} from "@tanstack/react-query";
+import axios from "axios";
 
 const DoingList = () => {
 
-    return(
-        <div className={"col-span-3 gap-3 flex flex-col p-2 h-[85vh] backdrop-blur-sm shadow-md rounded-2xl bg-white/30 "}>
+    async function fetchPosts() {
+        const {data} = await axios.get('https://fakestoreapi.com/products')
+
+        return data;
+
+    }
+
+
+    const {data, error, isLoading, isError} = useQuery(['posts'], fetchPosts);
+
+
+    if (isLoading) {
+        return <div>.... loading</div>
+    }
+
+
+    if (isError) {
+        return 'An error has occurred: ' + error.message
+    }
+    return (
+        <div
+            className={"col-span-3 gap-3 flex flex-col p-2 h-[85vh] backdrop-blur-sm shadow-md rounded-2xl bg-white/30 "}>
             <h1 className={"font-black text-xl text-gray-600 mb-5"}>
                 In Progress
             </h1>
+            <div className={"flex flex-col gap-8 overflow-y-scroll    max-w-3xl"}>
+                {
+                    console.log(data)
+                }
+                {
+                    data.map(item => (
+                        <div className={"flex flex-col gap-3 backdrop-blur-sm shadow-md rounded-2xl bg-white/30 p-3 m-2"} key={item.id} >
+                            <div className={"bg-red-100 max-w-max py-1 px-3 rounded-2xl"}>
+                                {item.category}
+                            </div>
+                            <img src={`${item.image}`} alt={item.category} className={'w-[50%]'}/>
+                            <div>
+                                {item.title}
+                            </div>
+                            <div className={"flex flex-row gap-3"}>
+                                <div>rate:{item.rating.rate}</div> <div>{item.price}$</div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 
